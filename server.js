e5f04f2e7cd8fd76433fb813ec21f0ed779f1f74
Artem <electrum181@gmail.com>
2019-03-11 17:32:23 +0300

    Cloud/Container fix support + correct package

--- server.js
+++ server.js
@@ -3,20 +3,11 @@ var timeEnd = Date.now(), ping = 0;
 const PATH = require("path"),
 
       EXPRESS = require("express"),
-      ROUTER_PING = require("express-router-ping"),
 
       BODY_PARSER = require("body-parser"),
       COMPRESSION = require("compression"),
       HELMET = require("helmet"),
       APP = EXPRESS();
-
-var date = () => {
-  var GetDate = new Date(),
-      Days = ["вс", "пн", "вт", "ср", "чт", "пт", "сб"],
-      Day = Days[GetDate.getDay()];
-  return `${Day} ${GetDate.getHours()}:${GetDate.getMinutes()}:${GetDate.getSeconds()}`;
-
-};
 
 APP.enable("trust proxy");
 
@@ -35,35 +26,32 @@ APP.use(BODY_PARSER.json());
 APP.use(BODY_PARSER.urlencoded({ extended: true }));
 
 APP.use("/editor/pony-female", EXPRESS.static(__dirname));
-//APP.use("/editor/pony-female", ROUTER_PING);
-
 APP.get("/editor/pony-female", (req, res) => {
   res.render("editor");
-  console.log(`${req.ip} | ${decodeURIComponent(req.originalUrl.replace(/\+/g," "))} | ${date()}`);
+  console.log(`${req.ip} | ${decodeURIComponent(req.originalUrl.replace(/\+/g," "))} | ${Date()}`);
 });
 
 APP.use("/home", EXPRESS.static(__dirname));
 APP.get("/home", (req, res) => {
   res.render("intro");
-  console.log(`${req.ip} | ${decodeURIComponent(req.originalUrl.replace(/\+/g," "))} | ${date()}`);
+  console.log(`${req.ip} | ${decodeURIComponent(req.originalUrl.replace(/\+/g," "))} | ${Date()}`);
 });
 
 APP.use("/about", EXPRESS.static(__dirname));
 APP.get("/about", (req, res) => {
   res.render("about");
-  console.log(`${req.ip} | ${decodeURIComponent(req.originalUrl.replace(/\+/g," "))} | ${date()}`);
+  console.log(`${req.ip} | ${decodeURIComponent(req.originalUrl.replace(/\+/g," "))} | ${Date()}`);
 });
 
 APP.use("/help", EXPRESS.static(__dirname));
 APP.get("/help", (req, res) => {
   res.render("help");
-  console.log(`${req.ip} | ${decodeURIComponent(req.originalUrl.replace(/\+/g," "))} | ${date()}`);
+  console.log(`${req.ip} | ${decodeURIComponent(req.originalUrl.replace(/\+/g," "))} | ${Date()}`);
 });
 
 APP.get("/", (req, res) => { res.redirect("/home"); });
-//APP.get("/editor/pony-female/pingInfo", (req, res) => { res.send({ ping: ping }); });
 
-APP.listen(process.env.PORT || 3000, (err) => {
+APP.listen(5000, (err) => {
   if(err){ return console.log("Happened something bad!", err); }
-  console.log(`Server ready, port: ${process.env.PORT || 3000} | ${Date.now() - timeEnd} msec`);
+  console.log(`Server ready, port: ${5000} | ${Date.now() - timeEnd} msec`);
 });
