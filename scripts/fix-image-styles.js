883314ae2cf10e4570b04d1c0990ab93200e68cf
Artem <electrum181@gmail.com>
2021-08-18 19:13:51 +0300

    Nextjs images style minimizer

---
+++ scripts/fix-image-styles.js
@@ -0,0 +1,80 @@
+const { readdirSync, readFileSync, writeFileSync } = require('fs')
+
+const css = `
+<style>
+  .__image {
+    display: inline-block;
+    max-width: 100%;
+    overflow: hidden;
+    position: relative;
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  .__image div {
+    box-sizing: border-box;
+    display: block;
+    max-width: 100%;
+  }
+
+  .__image div img {
+    max-width: 100%;
+    display: block;
+    margin: 0;
+    border: none;
+    padding: 0;
+  }
+
+  .__image > img, .__image nosrcipt img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    box-sizing: border-box;
+    padding: 0;
+    border: none;
+    margin: auto;
+    display: block;
+    width: 0;
+    height: 0;
+    min-width: 100%;
+    max-width: 100%;
+    min-height: 100%;
+    max-height: 100%;
+  }
+</style>`.replace(/(\n| )/g, '')
+
+function fixFolder(url) {
+  const files = readdirSync(url).filter(file => /\.html/.test(file))
+
+  for (const file of files) {
+    const data = readFileSync(url + '/' + file, 'utf8')
+      .replace(
+        /style="display:inline-block;max-width:100%;overflow:hidden;position:relative;box-sizing:border-box;margin:0"/g,
+        'class="__image"'
+      )
+      .replace(
+        / style="box-sizing:border-box;display:block;max-width:100%"/g,
+        ''
+      )
+      .replace(
+        / style="max-width:100%;display:block;margin:0;border:none;padding:0"/g,
+        ''
+      )
+      .replace(
+        / style="position:absolute;top:0;left:0;bottom:0;right:0;box-sizing:border-box;padding:0;border:none;margin:auto;display:block;width:0;height:0;min-width:100%;max-width:100%;min-height:100%;max-height:100%"/g,
+        ''
+      )
+
+    writeFileSync(
+      url + '/' + file,
+      data.replace('</head>', css + '</head>'),
+      'utf8'
+    )
+  }
+}
+
+fixFolder('./.next/server/pages')
+fixFolder('./.next/server/pages/en')
+fixFolder('./.next/server/pages/ru')
