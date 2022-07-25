c8980101553a6428995b34f5ce108b87318b165a
Electrum18 <electrum181@gmail.com>
2022-07-25 12:58:14 +0300

    Production error test 2

--- components/editor/player/index.jsx
+++ components/editor/player/index.jsx
@@ -1,14 +1,15 @@
-import Accessories from './parts/accessories'
-import Body from './parts/body'
-import Wings from './parts/wings'
-import Skeleton from './skeleton'
+import Accessories from "./parts/accessories";
+import Body from "./parts/body";
+import Wings from "./parts/wings";
+import Skeleton from "./skeleton";
 
 export default function Player(props) {
   return (
     <group scale={0.1} dispose={null} {...props}>
+      <Skeleton />
       <Body />
       <Accessories />
       <Wings />
     </group>
-  )
+  );
 }
